422125e1075d212432b290ec35d03272494f5643
Adam Hitchcock <adam@northisup.com>
2012-01-08 23:47:58 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='singletonmixin',
+      version='1.0',
+      description='Here\'s a quite complete Singleton mixin class for Python, originally from http://www.garyrobinson.net/2004/03/python_singleto.html',
+      author='Gary Robinson',
+      url='http://github.com/northisup/singletonmixin',
+     )
